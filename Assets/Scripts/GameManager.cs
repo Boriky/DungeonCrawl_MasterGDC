3ce203b33cda1f552e1f5fd293fdbbb03f43ecbc
DESKTOP-GTU3TVE\Fama0 <fama0501@hotmail.com>
2017-06-09 15:54:03 +0200

    Script commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private Room roomInstance;
+    public Room roomPrefab;
+
+	// Use this for initialization
+	void Start () {
+        BeginGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void BeginGame()
+    {   
+
+        roomInstance = Instantiate(roomPrefab) as Room;
+
+        //TEMP: get a reference to the door of the previous room and pass the coordinates of the adjacent tile
+        //TEMP: random initial direction
+       StartCoroutine(roomInstance.Generate(new IntVector2(-1,-1), Directions.Direction.North));
+    }
+}
