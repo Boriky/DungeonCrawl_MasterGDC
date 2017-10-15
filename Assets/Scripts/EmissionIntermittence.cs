0184755e6c1b735447b767a5bb2401c6cb5eb72e
Riccardo Rizzo <rizzoricky@gmail.com>
2017-10-16 00:49:44 +0200

    Emissive materials, floating and rotating objects in main scene

--- Assets/Scripts/EmissionIntermittence.cs
+++ Assets/Scripts/EmissionIntermittence.cs
@@ -8,15 +8,37 @@ public class EmissionIntermittence : MonoBehaviour
     [SerializeField] float m_lowestEmission = 0.0f;
     [SerializeField] float m_maxEmission = 0.5f;
     [SerializeField] float m_intermittenceVelocity = 0.1f;
-	
+
+    Texture2D emissionTexture = null;
+
+    private void Start()
+    {
+        MeshRenderer m_meshRenderer = GetComponent<MeshRenderer>();
+        Material material = m_meshRenderer.material;
+        emissionTexture = (Texture2D)Instantiate(material.GetTexture("_Illum"));
+        m_meshRenderer.material.SetTexture("_Illum", emissionTexture);
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
         MeshRenderer m_meshRenderer = GetComponent<MeshRenderer>();
         Material material = m_meshRenderer.material;
         float emission = m_lowestEmission + Mathf.PingPong(Time.time * m_intermittenceVelocity, m_maxEmission);
-        Color baseColor = material.color;
+        
+        /*Color baseColor = material.color;
         Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
-        material.SetColor("_Emission", finalColor);	
+        material.SetColor("_Emission", finalColor);*/
+
+        Color[] colorArray = emissionTexture.GetPixels();
+
+        for (int index = 0; index < colorArray.Length; ++index)
+        {
+            Color color = colorArray[index];
+            colorArray[index] = new Color(0, 0, 0, emission); 
+        }
+
+        emissionTexture.SetPixels(colorArray);
+        emissionTexture.Apply();          
 	}
 }
---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    [SerializeField]
+    float m_scaleFactor = 0.0f;
+    [SerializeField]
+    float m_runningTime = 0.0f;
+    [SerializeField]
+    bool m_isRotating = false;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector3 newLocation = transform.position;
+        float deltaHeight = Mathf.Sin(m_runningTime + Time.deltaTime) - Mathf.Sin(m_runningTime);
+        newLocation.y += deltaHeight * m_scaleFactor;
+        m_runningTime += Time.deltaTime;
+        transform.position = newLocation;
+
+        if (m_isRotating)
+        {
+            transform.Rotate(0, 6.0f * 10.0f * Time.deltaTime, 0);
+        }
+	}
+}
